82f8f089955892d3df3e96a46975a0b7f1c9dec1
N1AKAI <rakansubs@gmail.com>
2024-10-20 20:13:55 +0100

    Added TaxeSejourTrim entity

--- src/main/java/ma/aboulhoda/taxesejourproject/bean/taxesejourtrim/TaxeSejourTrim.java
+++ src/main/java/ma/aboulhoda/taxesejourproject/bean/taxesejourtrim/TaxeSejourTrim.java
@@ -1,4 +1,64 @@
 package ma.aboulhoda.taxesejourproject.bean.taxesejourtrim;
 
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
 public class TaxeSejourTrim {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate dateDeDebut;
+    private LocalDate dateDeFin;
+
+    @ManyToOne
+    @JoinColumn(name = "categorie_locale_id")
+    private CategorieLocale categorieLocale;
+
+    @ManyToOne
+    @JoinColumn(name = "taux_taxe_sejour_trim_id")
+    private TauxTaxeSejourTrim tauxTaxeSejourTrim;
+
+    public TaxeSejourTrim() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDateDeDebut() {
+        return dateDeDebut;
+    }
+
+    public void setDateDeDebut(LocalDate dateDeDebut) {
+        this.dateDeDebut = dateDeDebut;
+    }
+
+    public LocalDate getDateDeFin() {
+        return dateDeFin;
+    }
+
+    public void setDateDeFin(LocalDate dateDeFin) {
+        this.dateDeFin = dateDeFin;
+    }
+
+    public CategorieLocale getCategorieLocale() {
+        return categorieLocale;
+    }
+
+    public void setCategorieLocale(CategorieLocale categorieLocale) {
+        this.categorieLocale = categorieLocale;
+    }
+
+    public TauxTaxeSejourTrim getTauxTaxeSejourTrim() {
+        return tauxTaxeSejourTrim;
+    }
+
+    public void setTauxTaxeSejourTrim(TauxTaxeSejourTrim tauxTaxeSejourTrim) {
+        this.tauxTaxeSejourTrim = tauxTaxeSejourTrim;
+    }
 }
