2be4f41699159b3287af97051e09f6b0aaa77810
N1AKAI <rakansubs@gmail.com>
2024-10-20 20:13:43 +0100

    Added TauxTaxeSejourTrim entity

--- src/main/java/ma/aboulhoda/taxesejourproject/bean/taxesejourtrim/TauxTaxeSejourTrim.java
+++ src/main/java/ma/aboulhoda/taxesejourproject/bean/taxesejourtrim/TauxTaxeSejourTrim.java
@@ -1,4 +1,33 @@
 package ma.aboulhoda.taxesejourproject.bean.taxesejourtrim;
 
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
 public class TauxTaxeSejourTrim {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToMany(mappedBy = "tauxTaxeSejourTrim")
+    private List<TaxeSejourTrim> taxeSejourTrimList;
+
+    public TauxTaxeSejourTrim() {}
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<TaxeSejourTrim> getTaxeSejourTrimList() {
+        return taxeSejourTrimList;
+    }
+
+    public void setTaxeSejourTrimList(List<TaxeSejourTrim> taxeSejourTrimList) {
+        this.taxeSejourTrimList = taxeSejourTrimList;
+    }
 }
